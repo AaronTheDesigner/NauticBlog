7e9e2d8cae3d481eb8ce62e065786c86977d352b
AaronTheDesigner <flux.charlotte@gmail.com>
2018-08-02 19:04:39 -0400

    blog utility complete

--- articles/models.py
+++ articles/models.py
@@ -6,11 +6,14 @@ class Article(models.Model):
   slug = models.SlugField()
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
-  #add in thubmnail later
+  thumb = models.ImageField(default="default.jpg", blank=True)
   #add in author later
   
   def __str__(self):
     return self.title
+
+  def snippet(self):
+    return self.body[:50] + '...'
 
   #python manage.py makemigrations
   #python manage.py migrate
