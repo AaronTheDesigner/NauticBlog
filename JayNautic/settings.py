341db900581d77f1cfe564af6b9baa79e45c15a5
AaronTheDesigner <flux.charlotte@gmail.com>
2018-08-08 18:31:34 -0400

    static

--- JayNautic/settings.py
+++ JayNautic/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath('assets')))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
