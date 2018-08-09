fbd8f35dce5fc93bc652700f453535e19fbb60fc
AaronTheDesigner <flux.charlotte@gmail.com>
2018-08-09 15:28:47 -0400

    reverting model and views

--- JayNautic/views.py
+++ JayNautic/views.py
@@ -1,6 +1,20 @@
-#from django.http import HttpResponse
-#from django.shortcuts import render
-#from articles.models import Article, Cover, Novel, Event, Podcast, Novella, Short
+from django.http import HttpResponse
+from django.shortcuts import render
+from articles.models import Article, Cover, Novel, Event, Podcast, Novella, Short
 
+def homepage(request):
+  #return HttpResponse('homepage')
+  covers = Cover.objects.all()
+  novels = Novel.objects.all()
+  events = Event.objects.all()
+  novellas = Novella.objects.all()
+  shorts = Short.objects.all()
+  
 
+  return render(request, 'homepage.html', {'covers': covers, 'novels': novels, 'events': events, 'novellas': novellas, 'shorts': shorts})
+
+def podcast(request):
+  #return HttpResponse('about')
+  podcasts = Podcast.objects.all()
+  return render(request, 'podcast.html', {'podcasts': podcasts})
 
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Article, Cover, Novel, Event, Podcast, Novella, Short
+from .models import Article
 from django.http import HttpResponse
 
 # Create your views here.
@@ -12,18 +12,4 @@ def article_detail(request, slug):
   article = Article.objects.get(slug=slug)
   return render(request, 'articles/article_detail.html', {'article': article})
 
-def homepage(request):
-  #return HttpResponse('homepage')
-  covers = Cover.objects.all()
-  novels = Novel.objects.all()
-  events = Event.objects.all()
-  novellas = Novella.objects.all()
-  shorts = Short.objects.all()
-  
 
-  return render(request, 'homepage.html', {'covers': covers, 'novels': novels, 'events': events, 'novellas': novellas, 'shorts': shorts})
-
-def podcast(request):
-  #return HttpResponse('about')
-  podcasts = Podcast.objects.all()
-  return render(request, 'podcast.html', {'podcasts': podcasts})
