db544f68b89a118f09023bd62a0f7e5279caae0d
AaronTheDesigner <flux.charlotte@gmail.com>
2018-08-09 15:04:54 -0400

    model fix attempt

--- JayNautic/views.py
+++ JayNautic/views.py
@@ -1,20 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from articles.models import Article, Cover, Novel, Event, Podcast, Novella, Short
+#from django.http import HttpResponse
+#from django.shortcuts import render
+#from articles.models import Article, Cover, Novel, Event, Podcast, Novella, Short
 
-def homepage(request):
-  #return HttpResponse('homepage')
-  covers = Cover.objects.all()
-  novels = Novel.objects.all()
-  events = Event.objects.all()
-  novellas = Novella.objects.all()
-  shorts = Short.objects.all()
-  
 
-  return render(request, 'homepage.html', {'covers': covers, 'novels': novels, 'events': events, 'novellas': novellas, 'shorts': shorts})
-
-def podcast(request):
-  #return HttpResponse('about')
-  podcasts = Podcast.objects.all()
-  return render(request, 'podcast.html', {'podcasts': podcasts})
 
