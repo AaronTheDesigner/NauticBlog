7ebe5ea35f16220ea0415dfb030fa2c9925bd552
AaronTheDesigner <flux.charlotte@gmail.com>
2018-08-11 17:41:44 -0400

    sigh

--- JayNautic/settings.py
+++ JayNautic/settings.py
@@ -14,8 +14,7 @@ import django_heroku
 import os
 import dj_database_url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+
 
 
 
@@ -82,13 +81,9 @@ WSGI_APPLICATION = 'JayNautic.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 
