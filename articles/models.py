4ed359271999e757df17110231f95111bdc107ce
aaron toliver <flux.charlotte@gmail.com>
2018-12-08 23:01:18 -0500

    facelift

--- articles/models.py
+++ articles/models.py
@@ -28,39 +28,17 @@ class Novel(models.Model):
   cover_1 = models.ImageField()
   blurb_1 = models.TextField()
 
-  title_2 = models.CharField(max_length=50)
-  cover_2 = models.ImageField()
-  blurb_2 = models.TextField()
-
-  title_3 = models.CharField(max_length=50)
-  cover_3 = models.ImageField()
-  blurb_3 = models.TextField()
-
 class Novella(models.Model):
   title_1 = models.CharField(max_length=50)
   cover_1 = models.ImageField()
   blurb_1 = models.TextField()
 
-  title_2 = models.CharField(max_length=50)
-  cover_2 = models.ImageField()
-  blurb_2 = models.TextField()
-
-  title_3 = models.CharField(max_length=50)
-  cover_3 = models.ImageField()
-  blurb_3 = models.TextField()
 
 class Short(models.Model):
   title_1 = models.CharField(max_length=50)
   cover_1 = models.ImageField()
   blurb_1 = models.TextField()
 
-  title_2 = models.CharField(max_length=50)
-  cover_2 = models.ImageField()
-  blurb_2 = models.TextField()
-
-  title_3 = models.CharField(max_length=50)
-  cover_3 = models.ImageField()
-  blurb_3 = models.TextField()
 
 class Event(models.Model):
   event_1 = models.CharField(max_length=50)
@@ -68,15 +46,6 @@ class Event(models.Model):
   description_1 = models.TextField()
   link_1 = models.URLField()
 
-  event_2 = models.CharField(max_length=50)
-  event_picture_2 = models.ImageField()
-  description_2 = models.TextField()
-  link_2 = models.URLField()
-
-  event_3 = models.CharField(max_length=50)
-  event_picture_3 = models.ImageField()
-  description_3 = models.TextField()
-  link_3 = models.URLField()
 
 class Podcast(models.Model):
   title = models.CharField(max_length=50)
